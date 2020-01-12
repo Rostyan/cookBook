bac0ff3e049640fd5f899df8da631ad021250da4
Rostyan <kondrat1310@gmail.com>
2020-01-12 21:08:37 +0200

    add delete function on server side

---
+++ src/server/utils/deleteRecipes.js
@@ -0,0 +1,11 @@
+const Reciepes = require('../model/recipes');
+
+module.exports.delete = function (req,res) {
+  
+  let id = req.body.id;
+
+  Reciepes.remove({_id: id})
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err));
+
+};
